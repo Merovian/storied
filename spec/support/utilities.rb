d9ef4afec255fbbf76bebff16ae04f830e2f0806
Will Cowen <will.cowen@gmail.com>
2013-01-30 10:01:00 -0500

    fix rspec utility

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,9 @@
-def dom_name(object, attribute)
+def dom_name(object, attribute='id')
   "#{object.class.to_s.downcase}_#{object.send(attribute).gsub(/[^0-9A-Za-z\-_]/, '_')}"
+rescue
+  if object.respond_to?('id')
+    "#{object.class.to_s.downcase}_#{object.id}"
+  else
+    "#{object.class.to_s.downcase}_#{object.to_s.gsub(/[^0-9A-Za-z\-_]/, '_')}"
+  end
 end
